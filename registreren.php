6182ee878928ac16bbc70b20c422d099932ab739
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-11 13:35:00 +0200

    dingen aangepast

--- registreren.php
+++ registreren.php
@@ -13,14 +13,27 @@
 </head>
 
 <body>
-   <form action="registreer_proces.php" method="post">
+   <form action="inloggen.php" method="post">
+      <p>Voornaam</p>
+      <input type="text" name="voornaam" id="" placeholder="Vul je voornaam in">
+      <p>Achternaam</p>
+      <input type="text" name="achternaam" id="" placeholder="Vul je achternaam in">
       <p>E-Mail</p>
-      <input type="text" name="email" id="" placeholder="Vul je username of email in">
+      <input type="text" name="email" id="" placeholder="Vul je email in">
       <p>Wachtwoord</p>
       <input type="text" name="wachtwoord" id="" placeholder="Vul je wachtwoord in">
+      <p>Geboortedatum</p>
+      <input type="date" name="geboortedatum" id="" placeholder="Vul je geboortedatum in">
+      <p>Telefoonnummer</p>
+      <input type="tel" name="telefoonnummer" id="" placeholder="Vul je telefoonnummer in">
+      <p>Adres</p>
+      <input type="text" name="adres" id="" placeholder="Vul je adres in">
+      <p>Postcode</p>
+      <input type="text" name="postcode" id="" placeholder="Vul je postcode in">
+      <p>Stad</p>
+      <input type="text" name="stad" id="" placeholder="Vul je stad in">
       <p>Wat is uw rol?</p>
-      <input type="radio" name="rol" id="klant">klant<br>
-      <input type="radio" name="rol" id="medewerker">medewerker<br>
+      <input type="text" name="rol" id="" placeholder="Vul je rol in">
       <button type="submit">Inloggen</button>
    </form>
 </body>

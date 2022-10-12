d267df7da186f5aa86c7d8f64c2a2378f3ca9d73
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-12 13:46:30 +0200

    aannpassingen

--- product_registreer_verwerk.php
+++ product_registreer_verwerk.php
@@ -3,42 +3,29 @@
 if (isset($_POST["submit"])) {
 
    if (
-      !empty($_POST["voornaam"])
-      || !empty($_POST["achternaam"])
-      && !empty($_POST["email"])
-      && !empty($_POST["wachtwoord"])
-      && !empty($_POST["geboortedatum"])
-      && !empty($_POST["telefoon"])
-      && !empty($_POST["adres"])
-      && !empty($_POST["postcode"])
-      && !empty($_POST["stad"])
-      && !empty($_POST["rol"])
+      !empty($_POST["naam"])
+      && !empty($_POST["prijs_per_kg"])
+      && !empty($_POST["smaak_van_de_week"])
+      && !empty($_POST["categorie"])
 
    ) {
       // als op registreer wordt gedrukt 
       if (isset($_POST['submit'])) {
 
-
-         $voornaam = $_POST['voornaam'];
-         $achternaam = $_POST['achternaam'];
-         $email = trim($_POST["email"]);
-         $wachtwoord = $_POST['wachtwoord'];
-         $geboortedatum = $_POST['geboortedatum'];
-         $telefoon = $_POST['telefoon'];
-         $adres = $_POST['adres'];
-         $postcode = $_POST['postcode'];
-         $stad = $_POST['stad'];
-         $rol = $_POST['rol'];
+         $naam = $_POST['naam'];
+         $prijs_per_kg = $_POST['prijs_per_kg'];
+         $smaak_van_de_week = $_POST['smaak_van_de_week'];
+         $categorie = $_POST['categorie'];
 
          //database connectie
 
          require 'classes/database.php';
-         $sql = "INSERT INTO user (voornaam, achternaam, email, wachtwoord, geboortedatum, telefoon, adres, postcode, stad, rol)
-                VALUES ('$voornaam', '$achternaam', '$email', '$wachtwoord', '$geboortedatum', '$telefoon', '$adres', '$postcode','$stad', '$rol')";
+         $sql = "INSERT INTO user (voornaam, prijs_per_kg, smaak_van_de_week, categorie)
+                VALUES ('$voornaam', '$prijs_per_kg', '$smaak_van_de_week', '$rol')";
 
          // Voer de INSERT INTO STATEMENT uit
          if (mysqli_query((new Database())->getConnection(), $sql)) {
-            header("location: user_overzicht.php");
+            header("location: product_overzicht.php");
          }
          mysqli_close($conn); // Sluit de database verbinding
       }

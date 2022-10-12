9ddd0c4673a204c94fdb51b2b7758f3dc3d0bbc1
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-12 10:34:00 +0200

    css aanpassingen en colomns in rows gezet

--- user_update.php
+++ user_update.php
@@ -14,7 +14,7 @@ if ($result = mysqli_query((new Database())->getConnection(), $sql)) {
 
    $user = mysqli_fetch_assoc($result);
 
-   var_dump($user);
+   //var_dump($user);
 
    if (is_null($user)) {
       header("location: user_overzicht.php");
@@ -35,27 +35,35 @@ if ($result = mysqli_query((new Database())->getConnection(), $sql)) {
 </head>
 
 <body>
+   <h1>Update uw gegevens</h1><br>
    <form action="user_update_verwerk.php" method="post">
-      <p>Voornaam</p>
-      <input type="text" name="voornaam" id="" class="form-control" value="<?php echo $user["voornaam"] ?>" placeholder="Vul je voornaam in" required>
-      <p>Achternaam</p>
-      <input type="text" name="achternaam" id="" class="form-control" value="<?php echo $user["achternaam"] ?>" placeholder="Vul je achternaam in" required>
-      <p>E-Mail</p>
-      <input type="email" name="email" id="" class="form-control" value="<?php echo $user["email"] ?>" placeholder="Vul je email in" required>
-      <p>Wachtwoord</p>
-      <input type="password" name="wachtwoord" id="" class="form-control" value="<?php echo $user["wachtwoord"] ?>" placeholder="Vul je wachtwoord in" required>
-      <p>Geboortedatum</p>
-      <input type="date" name="geboortedatum" id="" class="form-control" value="<?php echo $user["geboortedatum"] ?>" placeholder="Vul je geboortedatum in" required>
-      <p>Telefoonnummer</p>
-      <input type="tel" name="telefoonnummer" id="" class="form-control" value="<?php echo $user["telefoon"] ?>" placeholder="Vul je telefoonnummer in" required>
-      <p>Adres</p>
-      <input type="text" name="adres" id="" class="form-control" value="<?php echo $user["adres"] ?>" placeholder="Vul je adres in" required>
-      <p>Postcode</p>
-      <input type="text" name="postcode" id="" class="form-control" value="<?php echo $user["postcode"] ?>" placeholder="Vul je postcode in" required>
-      <p>Stad</p>
-      <input type="text" name="stad" id="" class="form-control" value="<?php echo $user["stad"] ?>" placeholder="Vul je stad in" required>
-      <p>Wat is uw rol?</p>
-      <input type="text" name="rol" id="" class="form-control" value="<?php echo $user["rol"] ?>" placeholder="Vul je rol in" required><br>
+      <div class="row">
+         <div class="col-md-6">
+            <p>Voornaam</p>
+            <input type="text" name="voornaam" id="" class="form-control" value="<?php echo $user["voornaam"] ?>" placeholder="Vul je voornaam in" required>
+            <p>Achternaam</p>
+            <input type="text" name="achternaam" id="" class="form-control" value="<?php echo $user["achternaam"] ?>" placeholder="Vul je achternaam in" required>
+            <p>E-Mail</p>
+            <input type="email" name="email" id="" class="form-control" value="<?php echo $user["email"] ?>" placeholder="Vul je email in" required>
+            <p>Wachtwoord</p>
+            <input type="password" name="wachtwoord" id="" class="form-control" value="<?php echo $user["wachtwoord"] ?>" placeholder="Vul je wachtwoord in" required>
+            <p>Geboortedatum</p>
+            <input type="date" name="geboortedatum" id="" class="form-control" value="<?php echo $user["geboortedatum"] ?>" placeholder="Vul je geboortedatum in" required>
+         </div>
+         <div class="col-md-6">
+            <p>Telefoonnummer</p>
+            <input type="tel" name="telefoonnummer" id="" class="form-control" value="<?php echo $user["telefoon"] ?>" placeholder="Vul je telefoonnummer in" required>
+            <p>Adres</p>
+            <input type="text" name="adres" id="" class="form-control" value="<?php echo $user["adres"] ?>" placeholder="Vul je adres in" required>
+            <p>Postcode</p>
+            <input type="text" name="postcode" id="" class="form-control" value="<?php echo $user["postcode"] ?>" placeholder="Vul je postcode in" required>
+            <p>Stad</p>
+            <input type="text" name="stad" id="" class="form-control" value="<?php echo $user["stad"] ?>" placeholder="Vul je stad in" required>
+            <p>Wat is uw rol?</p>
+            <input type="text" name="rol" id="" class="form-control" value="<?php echo $user["rol"] ?>" placeholder="Vul je rol in" required>
+         </div>
+      </div>
+      <br>
       <div class=" form-group">
          <button type="submit" class="shadow-sm btn btn-info" name="submit">Update gebruiker!</button>
          <a href="user_overzicht.php" class="shadow-sm btn btn-danger">Annuleer</a>

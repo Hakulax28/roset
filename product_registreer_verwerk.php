4da88362297e847ac7921734dd905da2a2aef72e
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-12 12:34:39 +0200

    bestanden toegevoegd

---
+++ product_registreer_verwerk.php
@@ -0,0 +1,46 @@
+<?php
+
+if (isset($_POST["submit"])) {
+
+   if (
+      !empty($_POST["voornaam"])
+      || !empty($_POST["achternaam"])
+      && !empty($_POST["email"])
+      && !empty($_POST["wachtwoord"])
+      && !empty($_POST["geboortedatum"])
+      && !empty($_POST["telefoon"])
+      && !empty($_POST["adres"])
+      && !empty($_POST["postcode"])
+      && !empty($_POST["stad"])
+      && !empty($_POST["rol"])
+
+   ) {
+      // als op registreer wordt gedrukt 
+      if (isset($_POST['submit'])) {
+
+
+         $voornaam = $_POST['voornaam'];
+         $achternaam = $_POST['achternaam'];
+         $email = trim($_POST["email"]);
+         $wachtwoord = $_POST['wachtwoord'];
+         $geboortedatum = $_POST['geboortedatum'];
+         $telefoon = $_POST['telefoon'];
+         $adres = $_POST['adres'];
+         $postcode = $_POST['postcode'];
+         $stad = $_POST['stad'];
+         $rol = $_POST['rol'];
+
+         //database connectie
+
+         require 'classes/database.php';
+         $sql = "INSERT INTO user (voornaam, achternaam, email, wachtwoord, geboortedatum, telefoon, adres, postcode, stad, rol)
+                VALUES ('$voornaam', '$achternaam', '$email', '$wachtwoord', '$geboortedatum', '$telefoon', '$adres', '$postcode','$stad', '$rol')";
+
+         // Voer de INSERT INTO STATEMENT uit
+         if (mysqli_query((new Database())->getConnection(), $sql)) {
+            header("location: user_overzicht.php");
+         }
+         mysqli_close($conn); // Sluit de database verbinding
+      }
+   }
+}

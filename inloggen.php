c90b52bc32067b3a56f22e4426ab6bcddc44c963
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-13 08:42:32 +0200

    aanpassingen voor tests

--- inloggen.php
+++ inloggen.php
@@ -15,8 +15,8 @@
       <input type="text" name="email" id="" placeholder="Vul je username of email in">
       <p>Wachtwoord</p>
       <input type="text" name="wachtwoord" id="" placeholder="Vul je wachtwoord in">
-      <p>Wat is uw rol?</p>
-      <input type="text" name="rol" id=""><br>
+      <!--<p>Wat is uw rol?</p>
+      <input type="text" name="rol" id=""><br>-->
       <button type="submit">Inloggen</button>
       <a href="registreren.php">Klik hier om te registreren.</a>
       <a href="index.php">Klik hier om terug te gaan.</a>

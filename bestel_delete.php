4b1267f27cd944dcef02c647da80887d57fd1a43
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-13 11:03:30 +0200

    aangemaakt en aangepast

---
+++ bestel_delete.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+print_r($_GET["id"]);
+
+require 'classes/database.php';
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM orders WHERE id = $id";
+
+if (mysqli_query((new Database())->getConnection(), $sql)) {
+   header("location: user_overzicht.php");
+}

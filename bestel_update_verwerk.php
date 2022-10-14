cf5a8f1c6e748c091e1226040f3d769602806845
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-14 08:52:35 +0200

    aanpassingen

--- bestel_update_verwerk.php
+++ bestel_update_verwerk.php
@@ -3,15 +3,19 @@
 if (isset($_POST["submit"])) {
 
    $id = $_GET["ID"];
+   echo $id;
    if (
-      !empty($_POST["oppak"])
+      !empty($_POST["user"])
+      && !empty($_POST["product"])
+      && !empty($_POST["oppak"])
       && !empty($_POST["bezorg"])
       && !empty($_POST["ontvang"])
 
    ) {
 
       //allemaal moeten ze true zijn
-
+      $user = $_POST['user'];
+      $product = $_POST['product'];
       $oppak = $_POST["oppak"];
       $bezorg = $_POST['bezorg'];
       $ontvang = $_POST['ontvang'];
@@ -19,6 +23,8 @@ if (isset($_POST["submit"])) {
       //database connectie
       require 'classes/database.php';
       $sql = "UPDATE orders SET 
+         gebr_id = '$user',
+         product_id = '$product',
          oppak = '$oppak',
          bezorg = '$bezorg', 
          ontvang = '$ontvang' WHERE ID = '$id'  ";

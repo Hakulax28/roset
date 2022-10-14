c08302d88609fad3edc07821f19b324f8bbdc2ad
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-14 07:58:00 +0200

    aanpassingen gemaakt

--- bestel_update.php
+++ bestel_update.php
@@ -16,14 +16,6 @@ if ($result = mysqli_query((new Database())->getConnection(), $sql)) {
       header("location: bestel_overzicht.php");
    }
 }
-$sql = "SELECT * FROM user";
-$result = mysqli_query((new Database())->getConnection(), $sql);
-$all_users = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-$sql = "SELECT * FROM product";
-$result = mysqli_query((new Database())->getConnection(), $sql);
-$all_products = mysqli_fetch_all($result, MYSQLI_ASSOC);
-?>
 ?>
 
 <!DOCTYPE html>
@@ -42,25 +34,19 @@ $all_products = mysqli_fetch_all($result, MYSQLI_ASSOC);
    <h1>Update de bestelling</h1><br>
    <form action="bestel_update_verwerk.php?id=<?php echo $id; ?>" method="post">
       <div class="row mx-auto">
-
-
          <div class="col-md-3 mx-auto">
             <p>Opgepakt</p>
             <input type="datetime-local" name="oppak" id="oppak" class="form-control" value="<?php echo $order["oppak"] ?>" disabled><br>
             <p>Bezorgd</p>
             <input type="datetime-local" name="bezorg" id="bezorg" class="form-control" value="<?php echo $order["bezorg"] ?>" placeholder="" required><br>
          </div>
-         <div class="col-md-3 mx-auto">
-            <p>Bezorgd</p>
-            <input type="datetime-local" name="bezorg" id="bezorg" class="form-control" value="<?php echo $order["bezorg"] ?>" placeholder="" required><br>
-         </div>
       </div>
       <div class="col-md-3 mx-auto">
          <p>Ontvangen</p>
-         <input type="text" name="ontvang" id="ontvang" class="form-control" value="<?php echo $order["ontvang"] ?>" placeholder="Vul de naam hoeveelheid in" required><br>
+         <input type="text" name="ontvang" id="ontvang" class="form-control" value="<?php echo $order["ontvang"] ?>" placeholder="Vul de hoeveelheid in" required><br>
       </div><br>
       <div class=" form-group">
-         <button type="submit" class="shadow-sm btn btn-info" name="submit">Update bestelling!</button>
+         <button type="submit" class="shadow-sm btn btn-info" name="submit">Update gebruiker!</button>
          <a href="bestel_overzicht.php" class="shadow-sm btn btn-danger">Annuleer</a>
       </div><br>
 

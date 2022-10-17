c06c09551bffa3b24b6c35c331427ca8edd31889
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-17 18:44:41 +0200

    bestand toegevoegd en aanpassigen gemaakt

--- bestel_overzicht.php
+++ bestel_overzicht.php
@@ -24,7 +24,7 @@ if ($result = mysqli_query((new Database())->getConnection(), $sql)) {
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/style_index.css">
    <title>Roset</title>
 </head>
 

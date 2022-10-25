80ccf8c56973884b3db7d16fd3c1cebbd55f248b
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-25 13:31:51 +0200

    aanpassing

--- bestelling.php
+++ bestelling.php
@@ -4,6 +4,8 @@
 
 $sql = "SELECT * FROM product ORDER BY naam ASC, naam DESC";
 
+//$sql = "SELECT * FROM product WHERE smaak_van_de_week=1";
+
 if ($result = mysqli_query((new Database())->getConnection(), $sql)) {
    $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
@@ -65,7 +67,7 @@ if ($result = mysqli_query((new Database())->getConnection(), $sql)) {
       </header>
       <aside class="s2">
          <h3>Smaak van de dag</h3>
-         <p><img src="image/<?php echo $product["foto"] + $product["smaak_van_de_week"] ?>.jpg"></p>
+         <p><img src="image/<?php $sql = "SELECT * FROM product WHERE smaak_van_de_week=1"; ?>.jpg"></p>
       </aside>
       <article class="bestel">
          <?php foreach ($products as $product) : ?>
@@ -78,6 +80,9 @@ if ($result = mysqli_query((new Database())->getConnection(), $sql)) {
       <aside class="s3">
          <h3>Populaire smaken</h3>
          <br>
+         <img src="image/<?php $sql = "SELECT * FROM product WHERE smaak_van_de_week"; ?>.jpg">
+         <img src="image/<?php $sql = "SELECT * FROM product WHERE smaak_van_de_week"; ?>.jpg">
+         <img src="image/<?php $sql = "SELECT * FROM product WHERE smaak_van_de_week"; ?>.jpg">
       </aside>
       <footer class="under">
          <section class="se2">
@@ -88,7 +93,13 @@ if ($result = mysqli_query((new Database())->getConnection(), $sql)) {
          <section class="se2">
             <h3>Bestelingen</h3>
          </section>
-         <section class="se2"><a href="index.php">Ga hier terug</a></section>
+         <section class="se2"><a href="index.php">
+               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
+                  <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
+                  <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z" />
+               </svg>
+               Ga hier terug</a>
+         </section>
       </footer>
    </div>
 

392648dbd3dbb594a3c49209c2b2df9ba1e84ebf
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-28 13:38:32 +0200

    aanpassing

--- winkelwagen.php
+++ winkelwagen.php
@@ -138,10 +138,10 @@ $all_products = mysqli_fetch_all($result, MYSQLI_ASSOC);
                   </select>
                   <h1>Wil je het oppaken of laten bezorgen?</h1><br>
                   <!-- Deze checkboxen zouden de oppak of bezorging uitschakelen-->
-                  Oppakken<input type="checkbox" name="" id="" required>
-                  Bezorgen<input type="checkbox" name="" id="" required>
-                  <h2>Opgepakt op</h2>
-                  <input type="datetime-local" name="bezorg" id="bezorg" required><br>
+                  <p>Oppakken<input type="checkbox" name="" id="" required></p>
+                  <p>Bezorgen<input type="checkbox" name="" id="" required></p>
+                  <!--<h2>Opgepakt op</h2>
+                  <input type="datetime-local" name="bezorg" id="bezorg" required><br>-->
                   <h2>Bezorgd in</h2>
                   <input type="datetime-local" name="bezorg" id="bezorg" required><br>
                   <h2>Waar wil je het bezorgd?</h2>

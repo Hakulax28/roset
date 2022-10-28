399851356131940a367cb3f6fd66054c77bb8369
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-10-28 08:37:36 +0200

    even de login process aangepast voor sessions

--- login_proces.php
+++ login_proces.php
@@ -1,10 +1,17 @@
 <?php
 
+require '<classes>database.php';
+
+session_start();
+
+$_SESSION["temp_data"]["message"] = null;
+
+if (empty($_POST["email"]) && empty($_POST["wachtwoord"])) {
+    header("location: inloggen.php");
+}
 
 $email = $_POST["email"];
 $password = $_POST["wachtwoord"];
-
-require 'classes/database.php';
 
 $sql = "SELECT * FROM user WHERE email = '$email' ";
 
@@ -13,22 +20,30 @@ $result = mysqli_query((new Database())->getConnection(), $sql);
 if (!is_null($result)) {
     $user = mysqli_fetch_assoc($result); // 1 rij
 
+    if (is_null($user)) {
+        //gebruiker onbekend
+        header("location: inloggen.php");
+        //var_dump($user);
+        //die;
+    } else {
+        if ($user["wachtwoord"] == $_POST["wachtwoord"]) {
+            //de gebruiker met email en wachtwoord zijn bekend. YEAAH!!
 
-    if ($user["wachtwoord"] == $_POST["wachtwoord"]) {
-        //de gebruiker met email en wachtwoord zijn bekend. YEAAH!!
+            session_start();
 
-        session_start();
+            $_SESSION["email"] = $user["email"];
+            $_SESSION["is_logged_in"] = true;
+            $_SESSION["rol"] = $user["rol"];
 
-        $_SESSION["email"] = $user["email"];
-        $_SESSION["is_logged_in"] = true;
-        //$_SESSION["rol"] = $user["rol"];
-        header("location: index.php");
-    }
-    // De gebruiker moet bepaalde dingen doen als ze een klant/medewerker zijn.
-    if ($_SESSION["rol"] == "medewerker") {
-        echo "U kan nu alles doen";
-        header("location: index.php");
-    } else if ($_SESSION["rol"] == "klant") {
-        header("location: hoofdpagina.php");
+            var_dump($_SESSION);
+            //Hier bekijkt hij of degene die heeft ingelogd een gebruiker of personeel is.
+            if ($_SESSION["rol"] == "medewerker") {
+                echo "U kan nu alles doen";
+                header("location: index.php");
+            } else if ($_SESSION["rol"] == "gebruiker") {
+                echo "U kan alleen een melding registreren";
+                header("location: hoofdpagina.php");
+            }
+        }
     }
 }
